d4369e987431bcaf80bfd45761759d1b6a0dc1c1
Shuzhen Wang <shuzhenwang@google.com>
2023-07-17 14:46:04 -0700

    Camera: ndk: Fix missing onCaptureSequenceCompleted callback
    With HAL buffer manager, in case the HAL caches buffers, the camera
    device won't become idle until the cached buffers are returned by the
    HAL. In that case, onCaptureSequenceCompleted won't be triggered.
    
    Modify the implementation such that onCaptureSequenceCompleted won't be
    delayed until onIdle.
    
    Test: Camera CTS, vendor testing
    Bug: 289323712
    Change-Id: I4600170e392615d247b4453dd4fbcf90cc55752f
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- camera/ndk/impl/ACameraDevice.cpp
+++ camera/ndk/impl/ACameraDevice.cpp
@@ -1501,13 +1501,13 @@ CameraDevice::checkAndFireSequenceCompleteLocked() {
             if (lastFrameNumber <= completedFrameNumber) {
                 ALOGV("Mark sequenceId %d as sequence completed", sequenceId);
                 it->second.isSequenceCompleted = true;
-            }
 
+                // Send sequence completion callback
+                sendCaptureSequenceCompletedLocked(sequenceId, lastFrameNumber);
+            }
         }
 
         if (it->second.isSequenceCompleted && it->second.isInflightCompleted) {
-            sendCaptureSequenceCompletedLocked(sequenceId, lastFrameNumber);
-
             it = mSequenceLastFrameNumberMap.erase(it);
             ALOGV("%s: Remove holder for sequenceId %d", __FUNCTION__, sequenceId);
         } else {
@@ -1534,8 +1534,6 @@ CameraDevice::removeCompletedCallbackHolderLocked(int64_t lastCompletedRegularFr
                 lastCompletedRegularFrameNumber);
         if (lastFrameNumber <= lastCompletedRegularFrameNumber) {
             if (it->second.isSequenceCompleted) {
-                sendCaptureSequenceCompletedLocked(sequenceId, lastFrameNumber);
-
                 it = mSequenceLastFrameNumberMap.erase(it);
                 ALOGV("%s: Remove holder for sequenceId %d", __FUNCTION__, sequenceId);
             } else {
