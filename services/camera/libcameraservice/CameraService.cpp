94975b504895d7b0b895df5137e33b613ea23a83
Terminator-J <terminator_j@hotbutteredmonkey.com>
2025-03-25 14:20:31 -0700

    Fixup! libcameraservice: add TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB
    * Needed after cid I1aa27ae66ba897752fdbac43b2a6f9c94301ae71 "Propagate clientAttribution to CameraService::BasicClient"
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -4442,7 +4442,7 @@ status_t CameraService::BasicClient::notifyCameraOpening() {
             mSharedMode);
 
 #if defined (CAMERA_NEEDS_CLIENT_INFO_LIB) || defined (CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS)
-    gVendorCameraProviderService->setPackageName(mClientPackageName.c_str());
+    gVendorCameraProviderService->setPackageName(getPackageName().c_str());
 #endif
 
     return OK;
